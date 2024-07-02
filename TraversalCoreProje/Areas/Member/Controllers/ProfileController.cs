4de2459ad41409e31612e8293d66c40e29f5a772
furkan-karadag <furkan_karadag_fb@hotmail.com>
2024-07-02 23:45:44 +0300

    Added UserEditViewModel

--- TraversalCoreProje/Areas/Member/Controllers/ProfileController.cs
+++ TraversalCoreProje/Areas/Member/Controllers/ProfileController.cs
@@ -1,12 +1,31 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using TraversalCoreProje.Areas.Member.Models;
 
 namespace TraversalCoreProje.Areas.Member.Controllers
 {
+	[Area("Member")]
+	[Route("Member/[controller]/[action]")]
 	public class ProfileController : Controller
 	{
-		public IActionResult Index()
+		private readonly UserManager<AppUser> _userManager;
+
+        public ProfileController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+		[HttpGet]
+        public async Task <IActionResult> Index()
 		{
-			return View();
+			var values = await _userManager.FindByNameAsync(User.Identity.Name);
+			UserEditVİewModel userEditVİewModel = new UserEditVİewModel();
+			userEditVİewModel.name = values.Name;
+			userEditVİewModel.surname = values.Surname;
+			userEditVİewModel.phonenumber = values.PhoneNumber;
+			userEditVİewModel.mail=values.Email;
+			return View(userEditVİewModel);
 		}
 	}
 }
